aacff09ff707a6128a279837aa785685fa277515
RomanDovgii <romandovgiiwork@gmail.com>
2020-11-26 17:26:09 +0300

    removed debugging

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import {XIAOMI_MI_ROUTER_PATH} from "./utils/const"
 import {translateCurrentPage} from "./components/translate-current-page/translate-current-page";
 
 const currentLocation = window.location;
-console.log(`jopa`);
-console.log(currentLocation.pathname.includes(XIAOMI_MI_ROUTER_PATH));
 
 if (currentLocation.pathname.includes(XIAOMI_MI_ROUTER_PATH)) {
   translateCurrentPage(currentLocation);
